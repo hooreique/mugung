1ff7d7d8cf3d330bce1c799fff5ef5628fafe4b8
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-07-30 21:24:41 +0900

    Initial NixOS configuration

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = inputs: {
+    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
