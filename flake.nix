2d9a48fbd01e81ad842dccbd0a61e8926f1ac706
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-01 07:59:00 +0900

    26.05

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   outputs = inputs: {
     nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
