4284e07058160058e379aec0b890192fd8d48597
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-08 19:57:47 +0900

    refactor(nixos): remove duplicate Home Manager package

--- configuration.nix
+++ configuration.nix
@@ -73,10 +73,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = [ pkgs.home-manager ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
