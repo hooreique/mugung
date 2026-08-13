131b14f7eba30934573ef52094165f222a1b65a2
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-13 21:47:39 +0900

    adopt determinate nix (#2)

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+  inputs.determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+  inputs.determinate.inputs.nixpkgs.follows = "nixpkgs";
   outputs = inputs: {
     nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        inputs.determinate.nixosModules.default
         ./configuration.nix
       ];
     };
